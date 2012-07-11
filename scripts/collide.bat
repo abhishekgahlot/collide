eee0ba9b55861357c03b14b14f369aae4210eb4b
Scott Blum <dragonsinth@gmail.com>
2012-07-11 01:28:11 -0400

    Fixes to collide.bat - Handles spaces in current directory path - Replace 'rm' for 'del'

--- scripts/collide.bat
+++ scripts/collide.bat
@@ -20,9 +20,9 @@ set TMP_FILE=%TEMP%\collide-%RANDOM%.tmp
 echo %CONFIG% > %TMP_FILE%
 
 rem TODO: Make the ant build generate this string.
-set COLLIDE_CLASSPATH=%BASE%;%BASE%lib\gwt-user.jar;%BASE%lib\collide-server.jar;%BASE%lib\guava-12.0.jar;%BASE%lib\gson-2.2.1.jar;%BASE%lib\jsr305.jar;%BASE%lib\org.eclipse.jgit-1.3.0.201202151440-r.jar;%BASE%lib\waveinabox-import-0.3.jar
+set "COLLIDE_CLASSPATH=%BASE%;%BASE%lib\gwt-user.jar;%BASE%lib\collide-server.jar;%BASE%lib\guava-12.0.jar;%BASE%lib\gson-2.2.1.jar;%BASE%lib\jsr305.jar;%BASE%lib\org.eclipse.jgit-1.3.0.201202151440-r.jar;%BASE%lib\waveinabox-import-0.3.jar"
 
 rem Launch the server, setting up the appropriate classpath and config files.
-call %BASE%lib\vertx\bin\vertx.bat run bootstrap.js -conf "%TMP_FILE%" -cp "%COLLIDE_CLASSPATH%" %*
+call "%BASE%lib\vertx\bin\vertx.bat" run bootstrap.js -conf "%TMP_FILE%" -cp "%COLLIDE_CLASSPATH%" %*
 
-rm %TMP_FILE%
+del %TMP_FILE%
